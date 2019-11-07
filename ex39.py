0dcec947f12321bbe7fda12756a13aa999521aab
espinolaramd <espinolaramd@hartwick.edu>
2019-11-07 01:08:40 -0500

    Update

--- ex39.py
+++ ex39.py
@@ -2,3 +2,52 @@
 #11.06.19
 #Dictionaries, Oh Lovely Dictionaries.
 
+states = {
+    'Oregon': 'OR',
+    'Florida': 'FL',
+    'California': 'CA',
+    'New York': 'NY',
+    'Michigan': 'MI',
+}
+cities = {
+    'CA': 'San Francisco',
+    'MI': 'Detroit',
+    'FL': 'Jacksonville',
+}
+
+cities['NY'] = 'New York'
+cities['OR'] = 'Portland'
+
+print('-' * 10)
+print("NY state has: ", cities['NY'])
+print("OR state has: ", cities['OR'])
+
+print('-' * 10)
+print("Michigan's abbreviation is: ", states['Michigan'])
+print("Florida's abbreviation is: ",states['Florida'])
+
+print('-' * 10)
+print("Michigan has: ", cities[states['Michigan']])
+print("Florida has: ", cities[states['Florida']])
+
+print('-' * 10)
+for state, abbrev in list(states.items()):
+    print(f"{state} is a abbreviated {abbrev}")
+
+print('-' * 10)
+for abbrev, city in list(cities.items()):
+    print(f"{abbrev} has the city {city}")
+
+print('-' * 10)
+for state, abbrev in list(states.items()):
+    print(f"{state} state is a abbreviated {abbrev}")
+    print(f"and has city {cities[abbrev]}")
+
+print('-' * 10)
+state = states.get('Texas')
+
+if not state:
+    print("Sorry, no Texas.")
+
+city = cities.get('TX', 'Does not exist')
+print(f"The city for the state 'TX' is: {city}")
